f24120af97bc001e3449cc5f17c1d4a3de8bc332
Iago A. Barbosa <gliago_iago@hotmail.com>
2023-06-05 09:28:58 -0300

    adicionado um banner ad de exemplo

--- src/components/advertisement/advertisement.tsx
+++ src/components/advertisement/advertisement.tsx
@@ -1,8 +1,11 @@
+import './advertisement.scss';
+
 export const Advertisement = () => {
 
   return (
     <div className="ad-container">
       {/* Espaço reservado para a exibição dos anúncios */}
+      <div className="ad-exemple"></div>
     </div>
   );
 };
